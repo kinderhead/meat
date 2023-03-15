3157e59da4d2fd6518d926dc1747152851eb912a
kinderhead <67707412+kinderhead@users.noreply.github.com>
2023-03-15 10:24:47 -0400

    text

--- main.cpp
+++ main.cpp
@@ -7,8 +7,28 @@ int main(int, char**) {
     const cen::mix mix;
     const cen::ttf ttf;
     
-    cen::window window;
+    cen::window window("Atmosphere", cen::iarea{});
     cen::renderer renderer = window.make_renderer();
-    
+
+    const cen::font font32{"res/Roboto-Regular.ttf", 32};
+    auto text = renderer.make_texture(font32.render_blended("Hello world!", cen::colors::white));
+
     window.show();
+
+    bool running = true;
+    cen::event_handler handler;
+    while (running) {
+        while (handler.poll()) {
+            if (handler.is(cen::event_type::quit)) {
+                running = false;
+                break;
+            }
+        }
+
+        renderer.clear_with(cen::colors::black);
+
+        renderer.render(text, cen::fpoint{5, 5});
+
+        renderer.present();
+    }
 }
