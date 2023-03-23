8d735a65ea4785354d62b9631d0d6c22cc01ca76
kinderhead <kinderhead512@gmail.com>
2023-03-22 20:20:28 -0400

    lots of stuff

--- main.cpp
+++ main.cpp
@@ -1,34 +1,59 @@
 #include <iostream>
+
+#define CENTURION_NO_VULKAN
 #include "centurion.hpp"
 
-int main(int, char**) {
-    const cen::sdl sdl;
-    const cen::img img;
-    const cen::mix mix;
-    const cen::ttf ttf;
-    
-    cen::window window("Atmosphere", cen::iarea{});
-    cen::renderer renderer = window.make_renderer();
+int main(int, char**)
+{
+    int mousex = 0;
+    int mousey = 0;
 
-    const cen::font font32{"res/Roboto-Regular.ttf", 32};
-    auto text = renderer.make_texture(font32.render_blended("Hello world!", cen::colors::white));
+    try
+    {
+        const cen::sdl sdl;
+        const cen::img img;
+        const cen::mix mix;
+        const cen::ttf ttf;
+        
+        cen::window window("Atmosphere", cen::iarea(600, 800));
+        cen::renderer renderer = window.make_renderer();
 
-    window.show();
+        const cen::font font16{"res/Roboto-Regular.ttf", 16};
+        
+        auto bg = renderer.make_texture("res/bg.png");
 
-    bool running = true;
-    cen::event_handler handler;
-    while (running) {
-        while (handler.poll()) {
-            if (handler.is(cen::event_type::quit)) {
-                running = false;
-                break;
+        window.show();
+
+        bool running = true;
+        cen::event_handler handler;
+        while (running)
+        {
+            while (handler.poll())
+            {
+                if (handler.is(cen::event_type::quit))
+                {
+                    running = false;
+                    break;
+                }
+                else if (const auto* mouse_ev = handler.try_get<cen::mouse_motion_event>())
+                {
+                    mousex = mouse_ev->x();
+                    mousey = mouse_ev->y();
+                }
             }
+
+            renderer.clear_with(cen::colors::black);
+
+            renderer.render(bg, cen::irect(0, 0, 600, 800));
+            renderer.render(renderer.make_texture(font16.render_blended((std::string("Altitude: ") + std::to_string((800-mousey)*2)).c_str(), cen::colors::white)), cen::ipoint(480, 20));
+
+            renderer.present();
         }
-
-        renderer.clear_with(cen::colors::black);
-
-        renderer.render(text, cen::fpoint{5, 5});
-
-        renderer.present();
     }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << "\n";
+    }
+
+    return 0;
 }
